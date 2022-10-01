e8674808d0e2c01532296342eb0e67109849cdd6
Kumbbar <damirnasibullin8@gmail.com>
2022-10-01 17:02:03 +0500

    start project, authentication logic added (add logout)

--- chat/settings.py
+++ chat/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load .env file
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5hrupfrzat^q0=4p@w695+ynv9)@u_p4y)r8ik7bs^r1(0piv%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
