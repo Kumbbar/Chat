bd4ad5e473d1b8e92ef05d07604a1f07f31ea774
Kumbbar <damirnasibullin8@gmail.com>
2022-11-19 22:24:25 +0500

    0.3 docker logic started

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../users/static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'users/static/'), os.path.join(BASE_DIR, 'messages/static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ chat/dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.10
+WORKDIR /app
+ADD . /app
+RUN pip install --upgrade pip && pip install -r requirements.txt
